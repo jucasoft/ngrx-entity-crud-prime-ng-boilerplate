74b447e1b61c20bbd396f293b232816d6a5855e7
juca <juca@jucasoft.com>
2020-12-09 09:35:43 +0100

    server.js

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const path = require('path');
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
+const api = '/api/v1';
 // Serve static files....
 server.use(express.static(__dirname + '/dist/ngrx-entity-crud-prime-ng-boilerplate'));
 
-server.use('/api/v1', middlewares);
-server.use('/api/v1', router);
+server.use(api, middlewares);
+server.use(api, router);
 
 server.get('**', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/ngrx-entity-crud-prime-ng-boilerplate/index.html'));
