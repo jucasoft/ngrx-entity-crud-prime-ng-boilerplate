e6d21b8a00731894ea40dbd8895202093a7dc3ad
juca <juca@jucasoft.com>
2021-05-25 10:07:10 +0200

    aggiunto sezione commenti per testa relazioni

--- src/app/main/views/user/user-list/user-list.component.ts
+++ src/app/main/views/user/user-list/user-list.component.ts
@@ -32,7 +32,7 @@ export class UserListComponent implements OnInit {
     );
 
     this.store$.dispatch(
-      UserStoreActions.SearchRequest({queryParams: {_limit: 15}})
+      UserStoreActions.SearchRequest({queryParams: {_limit: 10}})
     );
 
     this.store$.dispatch(
