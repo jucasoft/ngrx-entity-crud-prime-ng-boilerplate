1da6a1bb8b2dfe84a69718a20ea73684abb8f9f2
juca <juca@jucasoft.com>
2021-08-13 12:35:50 +0200

    versione 12 funzionante.

--- src/app/main/models/vo/launch.ts
+++
@@ -1,8 +0,0 @@
-export class Launch {
-    public id: string = undefined;
-    /**
-     * metodo statico utilizzato per recuperare l'id dell'entita.
-     * @param item
-     */
-    static selectId: (item: Launch) => string = item => item.id;
-}
