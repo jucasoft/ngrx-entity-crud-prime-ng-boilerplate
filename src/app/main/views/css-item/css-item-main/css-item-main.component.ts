4d05aaa5624b72cd45c97c526b1cf7ea0825edfd
juca <juca@jucasoft.com>
2021-10-09 22:45:54 +0200

    l

--- src/app/main/views/css-item/css-item-main/css-item-main.component.ts
+++ src/app/main/views/css-item/css-item-main/css-item-main.component.ts
@@ -1,8 +1,11 @@
 import {Component, OnInit} from '@angular/core';
-import {Store} from '@ngrx/store';
-import {CssItemStoreActions, CssSourceStoreActions, RootStoreState} from '@root-store/index';
+import {select, Store} from '@ngrx/store';
+import {CssItemStoreActions, CssItemStoreSelectors, CssSourceStoreActions, CssSourceStoreSelectors, RootStoreState} from '@root-store/index';
 import {Actions} from 'ngrx-entity-crud';
 import {CssItem} from '@models/vo/css-item';
+import {Observable} from 'rxjs';
+import {CssSource} from '@models/vo/css-source';
+import {filter, map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-css-item-main',
@@ -11,11 +14,21 @@ import {CssItem} from '@models/vo/css-item';
 })
 export class CssItemMainComponent implements OnInit {
 
+  collection$:Observable<CssItem[]>
+  source$:Observable<CssSource>
+
   constructor(private readonly store$: Store<RootStoreState.State>) {
   }
 
   actions: Actions<CssItem> = CssItemStoreActions.actions;
   ngOnInit(): void {
+    this.collection$ = this.store$.pipe(
+      select(CssItemStoreSelectors.selectAll),
+      map((values:CssItem[]) => values.filter(value => !!value.varName))
+    )
+    this.source$ = this.store$.select(
+      CssSourceStoreSelectors.selectItem
+    );
   }
 
   loadData() {
@@ -25,5 +38,9 @@ export class CssItemMainComponent implements OnInit {
     this.store$.dispatch(
       CssSourceStoreActions.SelectRequest({queryParams: {}})
     );
+  }
+
+  generate(collection, source) {
+
   }
 }
